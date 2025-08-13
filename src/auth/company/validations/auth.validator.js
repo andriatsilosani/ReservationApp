c2153f358235ca0d7c009829137d432a6ac4691f
andriatsilosani1 <101892997+andriatsilosani@users.noreply.github.com>
2025-08-14 01:21:15 +0400

    add company registration

---
+++ src/auth/company/validations/auth.validator.js
@@ -0,0 +1,21 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
+export const companyRegisterSchema = Joi.object({
+    name: Joi.string().required(),
+    address: Joi.string(),
+    phone: Joi.string()
+        .pattern(/^\d{9}$/)
+        .required()
+        .messages({
+            "string.pattern.base": '"phone" must be exactly 9 digits',
+        }),
+    email: Joi.string().email().required(),
+    courtNum: Joi.number().integer().min(1).required(),
+    owner: Joi.string().custom((value, helpers) => {
+        if (!mongoose.Types.ObjectId.isValid(value)) {
+            return helpers.error("Invalid CourtId");
+        }
+        return value;
+    }),
+});
